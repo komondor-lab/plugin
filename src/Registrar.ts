7a9e36abd2f7d2f2c1f4026f2d6895ab66afd85f
Homa Wong <homawong@gmail.com>
2018-03-23 18:35:37 -0700

    flattern register signature

--- src/Registrar.ts
+++ src/Registrar.ts
@@ -1,20 +1,18 @@
 import { getSpy, getStub } from './interfaces'
 import { PluginUtil } from './pluginUtil'
 
-export type PluginRecord<T> = {
-  getSpy: getSpy<T>,
-  getStub: getStub<T>,
-  /**
-   * Determine can the plugin support this subject.
-   */
-  support(subject): boolean
-}
 export interface Registrar {
   /**
    * Register plugin.
    * @param actionType Action type to be handled by the plugin.
+   * @param support Determine can the plugin support this subject.
    */
-  register<T = any>(actionType: string, pluginRecord: PluginRecord<T>): void,
+  register<T = any>(
+    actionType: string,
+    support: (subject) => boolean,
+    getSpy: getSpy<T>,
+    getStub: getStub<T>
+  ): void,
   /**
    * Utility for plugin creation.
    */
