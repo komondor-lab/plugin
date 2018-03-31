12a590a7ca6f6c9db7b831a8752eafdb5eccb38d
Homa Wong <homawong@gmail.com>
2018-03-30 23:05:05 -0700

    fix tests

--- src/SpecExpectation.spec.ts
+++ src/SpecExpectation.spec.ts
@@ -4,29 +4,29 @@ import { createExpectation, createScopedCreateExpectation } from '.'
 
 describe('createExpectation', () => {
   test('create with meta', () => {
-    const s = createExpectation('type')
+    const s = createExpectation('type', 'name')
     const actual = s('payload', { a: 1 })
-    t.deepEqual(actual, { type: 'type', payload: 'payload', meta: { a: 1 } })
+    t.deepEqual(actual, { type: 'type', name: 'name', payload: 'payload', meta: { a: 1 } })
   })
   test('meta is optional', () => {
-    const s = createExpectation('type')
+    const s = createExpectation('type', 'name')
     const actual = s('payload')
-    t.deepEqual(actual, { type: 'type', payload: 'payload', meta: {} })
+    t.deepEqual(actual, { type: 'type', name: 'name', payload: 'payload', meta: {} })
   })
 })
 
 describe('createScopedCreateExpectation', () => {
   test('create with meta', () => {
     const createSatisfier = createScopedCreateExpectation('x')
-    const s = createSatisfier('type')
+    const s = createSatisfier('type', 'name')
     const actual = s('payload', { a: 1 })
-    t.deepEqual(actual, { type: 'x/type', payload: 'payload', meta: { a: 1 } })
+    t.deepEqual(actual, { type: 'x/type', name: 'name', payload: 'payload', meta: { a: 1 } })
   })
   test('meta is optional', () => {
     const createSatisfier = createScopedCreateExpectation('x')
-    const s = createSatisfier('type')
+    const s = createSatisfier('type', 'name')
     const actual = s('payload')
-    t.deepEqual(actual, { type: 'x/type', payload: 'payload', meta: {} })
+    t.deepEqual(actual, { type: 'x/type', name: 'name', payload: 'payload', meta: {} })
   })
 })
 
