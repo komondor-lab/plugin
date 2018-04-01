468fc5782e504e2ba606d3a9a8c5f22d46e5e044
Homa Wong <homawong@gmail.com>
2018-04-01 00:56:13 -0700

    fix: create correct expectation when baseMata and meta are both not defined

--- src/SpecExpectation.ts
+++ src/SpecExpectation.ts
@@ -1,7 +1,15 @@
-export function createExpectation(type: string, name: string, baseMeta: { [k: string]: any } = {}) {
-  return (payload?, meta?: { [k: string]: any }) => ({ type, name, payload, meta: { ...baseMeta, ...meta } })
+export function createExpectation(type: string, name: string, baseMeta?: { [k: string]: any }) {
+  return (payload?, meta?: { [k: string]: any }) => {
+    if (!baseMeta && !meta)
+      return { type, name, payload }
+    return { type, name, payload, meta: { ...baseMeta, ...meta } }
+  }
 }
 
 export function createScopedCreateExpectation(scope: string) {
-  return (subType: string, name: string, baseMeta: { [k: string]: any } = {}) => (payload?, meta?: { [k: string]: any }) => ({ type: `${scope}/${subType}`, name, payload, meta: { ...baseMeta, ...meta } })
+  return (subType: string, name: string, baseMeta?: { [k: string]: any }) => (payload?, meta?: { [k: string]: any }) => {
+    if (!baseMeta && !meta)
+      return { type: `${scope}/${subType}`, name, payload }
+    return { type: `${scope}/${subType}`, name, payload, meta: { ...baseMeta, ...meta } }
+  }
 }
