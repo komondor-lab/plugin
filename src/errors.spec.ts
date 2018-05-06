7fc8cc2c71bc2d004d164fd509131bd341105cf2
Homa Wong <homawong@gmail.com>
2018-05-05 19:55:59 -0700

    fix: remove mismatch logic (#19)
    * fix: remove mismatch logic
    
    Move it to komondor.
    Plugin shouldn't be concerned with it.
    
    * remove satisfier.

--- src/errors.spec.ts
+++ src/errors.spec.ts
@@ -1,8 +1,6 @@
 import t from 'assert'
-import a from 'assertron'
 
 import { SimulationMismatch } from '.'
-import { specAction } from './testUtil';
 
 const expectedAction = { type: 'function', name: 'invoke', payload: [0, 'a'] }
 const actualCallbackAction = { type: 'komondor', name: 'callback', payload: { a: 1 }, meta: { id: 3 } }
@@ -20,43 +18,4 @@ test('SimulationMismatch error, received action optional', () => {
   t.equal(err.specId, 'some id')
   t.equal(err.expected, expectedAction)
   t.equal(err.message, `Recorded data for 'some id' doesn't match with simulation. Expecting { type: 'function', name: 'invoke', payload: [0, 'a'] } but received undefined`)
-})
-
-describe('SimulationMismatch.mismatch()', () => {
-  test('type mismatch returns true', () => {
-    t(SimulationMismatch.mismatch(
-      specAction({ type: 'x' }),
-      specAction({ type: 'function' })
-    ))
-  })
-  test('name mismatch returns true', () => {
-    t(SimulationMismatch.mismatch(
-      specAction({ name: 'y' }),
-      specAction({ name: 'invoke' })
-    ))
-  })
-  test('payload mismatch returns true', () => {
-    t(SimulationMismatch.mismatch(
-      specAction({ payload: 1 }),
-      specAction({ payload: 2 })
-    ))
-  })
-  test('payload matches return false', () => {
-    a.false(SimulationMismatch.mismatch(
-      specAction({ payload: [1] }),
-      specAction({ payload: [1] })
-    ))
-  })
-  test('payload with function matches null', () => {
-    a.false(SimulationMismatch.mismatch(
-      specAction({ payload: [1, x => x + 1] }),
-      specAction({ payload: [1, null] })
-    ))
-  })
-  test('payload is error', () => {
-    a.false(SimulationMismatch.mismatch(
-      specAction({ payload: new Error('foo') }),
-      specAction({ payload: { message: 'foo' } })
-    ))
-  })
 })
--- src/errors.ts
+++ src/errors.ts
@@ -1,14 +1,9 @@
 import { BaseError } from 'make-error'
-import { createSatisfier } from 'satisfier'
 import { tersify } from 'tersify'
 
 import { SpecAction } from './interfaces'
-import { serializeAction } from './serializeAction'
 
 export class SimulationMismatch extends BaseError {
-  static mismatch(actual: SpecAction, expected: SpecAction) {
-    return !createSatisfier(expected).test(serializeAction(actual))
-  }
   // istanbul ignore next
   constructor(public specId: string, public expected: Partial<SpecAction>, public actual?: Partial<SpecAction>) {
     super(`Recorded data for '${specId}' doesn't match with simulation. Expecting ${tersify(expected, { maxLength: Infinity })} but received ${tersify(actual, { maxLength: Infinity })}`)
